fb316c5ae1c50bbfddd52d4435800aa7d25345fe
Wang Yang <1527638985@qq.com>
2021-06-23 22:59:28 +0800

    Navbar & searching done

---
+++ src/component/page/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div>
+      <h1>About This App</h1>
+      <p>App to search Github users</p>
+      <p>Learn from Brad Traversy Udemy course</p>
+      <p>By Wang Yang</p>
+    </div>
+  );
+};
